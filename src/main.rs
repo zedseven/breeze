494d6ab9d2512466d7c4505ea2f6365f9d197484
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2024-01-11 00:18:46 -0500

    feat(display): Implement the base for opening a window and displaying content.

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,20 @@ mod sent;
 use std::env::args;
 
 use anyhow::{anyhow, Context, Result as AnyhowResult};
+use gfx::{
+	format::{Depth, Srgba8},
+	Device,
+	Encoder,
+};
+use gfx_glyph::{ab_glyph::FontArc, GlyphBrushBuilder, Section, Text};
+use glutin::surface::GlSurface;
+use glutin_winit::GlWindow;
+use old_school_gfx_glutin_ext::{window_builder as old_school_gfx_glutin_ext_window_builder, Init};
+use winit::{
+	event::{Event, WindowEvent},
+	event_loop::{ControlFlow, EventLoop},
+	window::WindowBuilder,
+};
 
 use crate::sent::Presentation;
 
@@ -60,5 +74,97 @@ fn main() -> AnyhowResult<()> {
 
 	dbg!(&presentation);
 
-	Ok(())
+	let event_loop =
+		EventLoop::new().with_context(|| "unable to initialise the display backend")?;
+	event_loop.set_control_flow(ControlFlow::Wait);
+	// TODO: Use the contents of the first slide as the title?
+	let window_builder = WindowBuilder::new().with_title(file_path);
+
+	// I wanted to implement the renderer initialisation myself, but the myriad ways
+	// to do it without any consistency or documentation led me to just use the same
+	// approach that the `glyph_brush` examples use. Perhaps this can be revisited
+	// in the future.
+	// https://github.com/alexheretic/glyph-brush/blob/main/gfx-glyph/examples/paragraph.rs
+	let Init {
+		window,
+		gl_surface,
+		gl_context,
+		mut device,
+		mut factory,
+		mut color_view,
+		mut depth_view,
+		..
+	} = old_school_gfx_glutin_ext_window_builder(&event_loop, window_builder)
+		.build::<Srgba8, Depth>()
+		.map_err(|error| anyhow!(error.to_string()))
+		.with_context(|| "unable to build the window")?;
+
+	let font = FontArc::try_from_slice(include_bytes!(
+		"/home/zacc/typefaces/pro-fonts/PragmataPro/PragmataPro0.829/PragmataPro_Mono_R_liga_0829.\
+		 ttf"
+	))
+	.with_context(|| "unable to load the font")?;
+	let mut glyph_brush = GlyphBrushBuilder::using_font(font)
+		.initial_cache_size((1024, 1024))
+		.build(factory.clone());
+
+	let mut encoder: Encoder<_, _> = factory.create_command_buffer().into();
+
+	let font_size: f32 = 18.0;
+	let mut view_size = window.inner_size();
+
+	#[allow(clippy::wildcard_enum_match_arm)]
+	event_loop
+		.run(move |event, window_target| match event {
+			Event::AboutToWait => window.request_redraw(),
+			Event::WindowEvent { event, .. } => match event {
+				WindowEvent::CloseRequested => window_target.exit(),
+				WindowEvent::RedrawRequested => {
+					// Handle resizes
+					let window_size = window.inner_size();
+					if view_size != window_size {
+						window.resize_surface(&gl_surface, &gl_context);
+						old_school_gfx_glutin_ext::resize_views(
+							window_size,
+							&mut color_view,
+							&mut depth_view,
+						);
+						view_size = window_size;
+					}
+
+					encoder.clear(&color_view, [0.02, 0.02, 0.02, 1.0]);
+
+					let (width, height, ..) = color_view.get_dimensions();
+					let (width, height) = (f32::from(width), f32::from(height));
+					let scale = font_size * window.scale_factor() as f32;
+
+					// The section is all the info needed for the glyph brush to render a 'section'
+					// of text.
+					let text = "Test\n    Lorem ipsum dolor sit amet.";
+
+					let section = Section::default()
+						.add_text(
+							Text::new(text)
+								.with_scale(scale)
+								.with_color([0.9, 0.3, 0.3, 1.0]),
+						)
+						.with_bounds((width / 3.15, height));
+
+					glyph_brush.queue(&section);
+
+					// Draw the text
+					glyph_brush
+						.use_queue()
+						.draw(&mut encoder, &color_view)
+						.unwrap();
+
+					encoder.flush(&mut device);
+					gl_surface.swap_buffers(&gl_context).unwrap();
+					device.cleanup();
+				}
+				_ => {}
+			},
+			_ => {}
+		})
+		.with_context(|| "encountered an error during the event loop")
 }
